2ef4bc21a5de3bf5e08facc4da79e5e7ad085283
DatsunCo <danya200375d@gmail.com>
2024-02-08 14:41:30 +0300

    dev: init state manager

---
+++ src/data/Store.js
@@ -0,0 +1,8 @@
+import {makeAutoObservable} from "mobx";
+
+export default class Store {
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Store from './data/Store.js'
 import './index.css'
 
+export const Context = createContext(null)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Context.Provider value={{
+      store: new Store()
+    }}>
     <App />
-  </React.StrictMode>,
+  </Context.Provider>
 )
